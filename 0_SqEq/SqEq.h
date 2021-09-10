349d7c97d9709c106d94028d65a5fcb31cffdbf5
Tako-San <dxe13@mail.ru>
2021-09-10 19:01:23 +0300

    [SqEq] Added docs

--- 0_SqEq/SqEq.h
+++ 0_SqEq/SqEq.h
@@ -1,3 +1,14 @@
+/**
+ * @file SqEq.h
+ * @author tako
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <stdbool.h>
 
 #define SQEQ_ERROR 1
@@ -11,8 +22,8 @@ enum ResType { RT_ERROR = -1, RT_NO_ROOTS, RT_ONE_ROOT, RT_TWO_ROOTS, RT_INF_ROO
 /**
  * @brief Reading 3 coefficients from stdin
  * 
- * @param prompt message to user
- * @param coeffs array for coeffs, length must be >= 3
+ * @param[in] prompt message to user
+ * @param[out] coeffs array for coeffs, length must be >= 3
  * @return true if all OK
  * @return false on error
  */
@@ -21,16 +32,16 @@ bool read_coeffs(const char *prompt, double coeffs[COEFF_NUM]);
 /**
  * @brief Print squaree equations roots
  * 
- * @param res_type defines error/number of roots
- * @param results roots
+ * @param[in] res_type defines error/number of roots
+ * @param[in] results roots
  */
 void print_res(int res_type, const double results[MAX_ROOT_NUM]);
 
 /**
  * @brief Solving square equation
  * 
- * @param coeffs array with coefficients, length must must be >= 3
- * @param results array with roots, length must must be >= 2
+ * @param[in] coeffs array with coefficients, length must must be >= 3
+ * @param[out] results array with roots, length must must be >= 2
  * @return result type
  */
 int solve_sqeq(const double coeffs[COEFF_NUM], double results[MAX_ROOT_NUM]);
@@ -38,9 +49,9 @@ int solve_sqeq(const double coeffs[COEFF_NUM], double results[MAX_ROOT_NUM]);
 /**
  * @brief Solving linear equation
  * 
- * @param b coeff on x
- * @param c free member
- * @param x_ptr result pointer
+ * @param[in] b coeff on x
+ * @param[in] c free member
+ * @param[out] x_ptr result pointer
  * @return resutl type
  */
 int solve_linear(double b, double c, double *x_ptr);
@@ -48,8 +59,8 @@ int solve_linear(double b, double c, double *x_ptr);
 /**
  * @brief Compares two double numbers
  * 
- * @param n1 1st num
- * @param n2 2nd num
+ * @param[in] n1 1st num
+ * @param[in] n2 2nd num
  * @return int 
  */
 int is_equal(double n1, double n2);
