0160b5fc3ade58996f50b311a1ad158a24287bdd
Tako-San <dxe13@mail.ru>
2021-09-10 17:44:31 +0300

    [SqEq] Added comments

--- 0_SqEq/SqEq.h
+++ 0_SqEq/SqEq.h
@@ -6,8 +6,29 @@
 
 enum ResType { RT_ERROR = -1, RT_NO_ROOTS, RT_ONE_ROOT, RT_TWO_ROOTS, RT_INF_ROOTS };
 
-
+/**
+ * @brief Reading 3 coefficients from stdin
+ * 
+ * @param prompt message to user
+ * @param coeffs array for coeffs, length must be >= 3
+ * @return true if all OK
+ * @return false on error
+ */
 bool read_coeffs(const char *prompt, double coeffs[COEFF_NUM]);
 
+/**
+ * @brief Print squaree equations roots
+ * 
+ * @param res_type defines error/number of roots
+ * @param results roots
+ */
 void print_res(int res_type, const double results[MAX_ROOT_NUM]);
+
+/**
+ * @brief Solving square equation
+ * 
+ * @param coeffs array with coefficients, length must must be >= 3
+ * @param results array with roots, length must must be >= 2
+ * @return result type
+ */
 int solve_sqeq(const double coeffs[COEFF_NUM], double results[MAX_ROOT_NUM]);
