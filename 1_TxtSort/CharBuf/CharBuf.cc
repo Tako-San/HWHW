2b45d3991beb99f4b5f21ca4d7b6991316dcee61
Tako-San <dxe13@mail.ru>
2021-09-18 17:32:39 +0300

    [TxtSort] Added CharBuf lib

---
+++ 1_TxtSort/CharBuf/CharBuf.cc
@@ -0,0 +1,19 @@
+#include "CharBuf.hh"
+
+CharBuf *cb_init(CharBuf *cb, size_t elem_num) {
+  if (nullptr == cb)
+    return nullptr;
+
+  cb->buf = (char*)calloc(elem_num + 1, sizeof(char));
+  cb->size = elem_num;
+  return cb;
+}
+
+CharBuf *cb_destr(CharBuf *cb) {
+  if (nullptr == cb)
+    return nullptr;
+
+  free(cb->buf);
+  cb->size = 0;
+  return cb;
+}
---
+++ 1_TxtSort/CharBuf/CharBuf.hh
@@ -0,0 +1,31 @@
+#ifndef CHAR_BUF_HH
+#define CHAR_BUF_HH
+
+#include <stdlib.h>
+
+/**
+ * @brief Struct which keeps dynamic char array and it's size
+ */
+struct CharBuf {
+  char *buf;
+  size_t size;
+};
+
+/**
+ * @brief CharBuf constructor
+ * 
+ * @param cb CharBuf object
+ * @param elem_num number of elements in buffer
+ * @return pointer to input CharBuf object
+ */
+CharBuf *cb_init(CharBuf *cb, size_t elem_num);
+
+/**
+ * @brief CharBuf destructor
+ * 
+ * @param cb CharBuf object
+ * @return pointer to input CharBuf object
+ */
+CharBuf *cb_destr(CharBuf *cb);
+
+#endif // CHAR_BUF_HH
