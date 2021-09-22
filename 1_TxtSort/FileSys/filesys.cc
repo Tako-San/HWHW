72a57eb48454ad813bf173047f4253d073965d72
Tako-San <dxe13@mail.ru>
2021-09-22 12:20:39 +0300

    [TxtSort] Fixed some issues  * #12  * #13

---
+++ 1_TxtSort/FileSys/filesys.cc
@@ -0,0 +1,50 @@
+#include "filesys.hh"
+#include "trace.hh"
+
+ssize_t tfl_fsize(FILE *fp)
+{
+  if (nullptr == fp)
+    return -1;
+
+  if (-1 == fseek(fp, 0L, SEEK_END))
+    return -1;
+  ssize_t res = ftell(fp);
+
+  rewind(fp);
+  return res;
+}
+
+int file_to_buf(const char *fname, CharBuf *raw_data)
+{
+  FILE *fp = fopen(fname, "rb");
+  if (nullptr == fp)
+  {
+    tll_error("Error while opening file.\n");
+    return tll_exit_code();
+  }
+
+  ssize_t fsize = tfl_fsize(fp);
+  if (fsize < 0)
+  {
+    tll_error("Can't extract file size.\n");
+    return tll_exit_code();
+  }
+
+  if (nullptr == cb_init(raw_data, fsize))
+  {
+    tll_error("Error while opening file.\n");
+    return tll_exit_code();
+  }
+
+  tll_verbose("Reading from file\n");
+  if (fread(raw_data->buf, sizeof(char), fsize, fp) < fsize)
+  {
+    tll_error("Error while reading file.\n");
+    return tll_exit_code();
+  }
+
+  tll_verbose("Closing file: %s\n", fname);
+  fclose(fp);
+
+  return tll_exit_code();
+}
--- 1_TxtSort/main.cc
+++ 1_TxtSort/main.cc
@@ -3,10 +3,9 @@
 #include <string.h>
 
 #include "CharBuf.hh"
+#include "filesys.hh"
 #include "trace.hh"
 #include "tsl.hh"
-
-long file_size(FILE *fp);
 
 int main(int argc, char *argv[])
 {
@@ -19,34 +18,11 @@ int main(int argc, char *argv[])
 
   tll_verbose("Opening file: %s\n", argv[1]);
 
-  char *fname = argv[1];
-  FILE *fp = fopen(fname, "rb");
-  if (nullptr == fp)
-  {
-    tll_error("Error while opening file.\n");
-    return tll_exit_code();
-  }
-
-  size_t fsize = file_size(fp);
   CharBuf raw_data = {nullptr, 0};
-  if (nullptr == cb_init(&raw_data, fsize))
-  {
-    tll_error("Error while opening file.\n");
+  if (file_to_buf(argv[1], &raw_data) != 0)
     return tll_exit_code();
-  }
-
-  tll_verbose("Reading from file\n");
-  if (fread(raw_data.buf, sizeof(char), fsize, fp) < fsize)
-  {
-    tll_error("Error while reading file.\n");
-    return tll_exit_code();
-  }
-  
-  tll_verbose("Closing file: %s\n", argv[1]);
-  fclose(fp);
 
   StrArray parsed_data = tsl_split_lines(raw_data);
-
   for (size_t i = 0; i < parsed_data.size; ++i)
     printf("%.*s\n", (int)parsed_data.lines[i].size, parsed_data.lines[i].buf);
 
@@ -58,14 +34,4 @@ int main(int argc, char *argv[])
   cb_destr(&raw_data);
   free(parsed_data.lines);
   return tll_exit_code();
-}
-
-long file_size(FILE *fp)
-{
-  if (-1 == fseek(fp, 0L, SEEK_END))
-    return -1;
-  long res = ftell(fp);
-
-  rewind(fp);
-  return res;
 }
