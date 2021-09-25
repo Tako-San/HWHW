11c5bca6eb83c98a4859af1a842244732211314a
Tako-San <dxe13@mail.ru>
2021-09-26 00:10:59 +0300

    [Stack] Added init function closes #18

--- 2_Stack/main.cc
+++ 2_Stack/main.cc
@@ -1,32 +1,36 @@
-#include <stdio.h>
 #include "stack.hh"
+#include <stdio.h>
 
-define_stack(int)
-define_stack(double)
+define_stack(int);
+define_stack(double);
 
 int main()
 {
-  stack(int) * testStack = stk_new(int);
-  stk_push(testStack, 1);
-  stk_push(testStack, 2);
-  stk_push(testStack, 3);
-  stk_push(testStack, 4);
+  Stack(int) testStack{};
+  stk_init(int, &testStack);
 
-  while (!stk_is_empty(testStack))
-    printf("%d\n", stk_pop(testStack));
+  stk_push(&testStack, 1);
+  stk_push(&testStack, 2);
+  stk_push(&testStack, 3);
+  stk_push(&testStack, 4);
 
-  stk_destroy(testStack);
-  free(testStack);
+  while (!stk_is_empty(&testStack))
+    printf("%5d ", stk_pop(&testStack));
+
+  stk_destroy(&testStack);
 
   puts("");
 
-  stack(double) * dStack = stk_new(double);
+  Stack(double) *dStack = stk_new(double);
   stk_push(dStack, 12.5);
   stk_push(dStack, 24.5);
   stk_push(dStack, 9.12);
+  stk_push(dStack, 32.51);
 
   while (!stk_is_empty(dStack))
-    printf("%.2f\n", stk_pop(dStack));
+    printf("%5.2f ", stk_pop(dStack));
+
+  puts("");
 
   stk_destroy(dStack);
   free(dStack);
