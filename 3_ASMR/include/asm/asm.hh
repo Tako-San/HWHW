98d07c272e93f8486e052972c5838d53b0a4212d
Tako-San <dxe13@mail.ru>
2021-10-27 01:02:59 +0300

    [ASMR] Started implementation of tokenizer

---
+++ 3_ASMR/include/asm/asm.hh
@@ -0,0 +1,4 @@
+#ifndef __INCLUDE_ASM_ASM_HH__
+#define __INCLUDE_ASM_ASM_HH__
+
+#endif // __INCLUDE_ASM_ASM_HH__
---
+++ 3_ASMR/include/tokenizer/tokenizer.hh
@@ -0,0 +1,26 @@
+#ifndef __INCLUDE_TOKENIZER_TOKENIZER_HH__
+#define __INCLUDE_TOKENIZER_TOKENIZER_HH__
+
+namespace ASMR
+{
+
+class tokenizer
+{
+private:
+  /* data */
+public:
+  tokenizer(/* args */);
+  ~tokenizer();
+};
+
+tokenizer::tokenizer(/* args */)
+{
+}
+
+tokenizer::~tokenizer()
+{
+}
+
+} // namespace ASMR
+
+#endif // __INCLUDE_TOKENIZER_TOKENIZER_HH__
