3ef650c6162ade89eda1896aaa15aaa78a211809
Tako-San <dxe13@mail.ru>
2021-10-27 22:19:01 +0300

    [ASMR] Started implementation of assembler

--- 3_ASMR/tools/assembler/main.cc
+++ 3_ASMR/tools/assembler/main.cc
@@ -1,6 +1,8 @@
 #include <cstdio>
 
-#include <tsl.hh>
+#include "filesys.hh"
+#include "trace.hh"
+#include "tsl.hh"
 
 int main(int argc, char **argv)
 {
@@ -10,5 +12,19 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  CharBuf raw_input{};
+  int ftb_res = file_to_buf(argv[1], &raw_input);
+  if (ftb_res != 0)
+  {
+    tll_error("Error while opening file %s\n", argv[1]);
+    return ftb_res;
+  }
+
+  StrArray sep_input = tsl_split_lines(raw_input);
+
+  sa_print(sep_input, stdout);
+
+  sa_destr(&sep_input);
+  cb_destr(&raw_input);
   return 0;
 }
