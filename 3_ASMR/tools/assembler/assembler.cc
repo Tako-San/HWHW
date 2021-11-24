00b9642ee10c0d5a9a2fed1214a91ea85fd9317d
Tako-San <dxe13@mail.ru>
2021-11-24 17:47:51 +0300

    [ASMR] minor

--- 3_ASMR/tools/assembler/assembler.cc
+++ 3_ASMR/tools/assembler/assembler.cc
@@ -4,7 +4,9 @@
 
 #include "assembler.hh"
 
-int assembler_main(const char * input_file, const char * output_file)
+void assembly(StrArray input_buf, int32_t *output_buf);
+
+int assembler_main(const char *input_file, const char *output_file)
 {
   CharBuf raw_input{};
   int ftb_res = file_to_buf(input_file, &raw_input);
@@ -16,10 +18,28 @@ int assembler_main(const char * input_file, const char * output_file)
 
   StrArray sep_input = tsl_split_lines(raw_input);
 
+  int32_t *output_buf = (int32_t *)calloc(sep_input.size, sizeof(int32_t));
+  if (nullptr == output_buf)
+  {
+    tll_error("Output buffer allocation error\n");
+    return 1;
+  }
+
+  assembly(sep_input, output_buf);
   sa_print(sep_input, stdout);
 
   sa_destr(&sep_input);
   cb_destr(&raw_input);
 
   return 0;
+}
+
+void assembly(StrArray input_buf, int32_t *output_buf)
+{
+  for (size_t i = 0, j = 0; i < input_buf.size; ++i)
+  {
+    // output_buf[j] = ASMR::get_opcode(input_buf.lines[i]);
+    // if (output_buf[j] != ASMR::EMPTY_OP)
+    //   ++j;
+  }
 }
